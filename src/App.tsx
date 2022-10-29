cb53c8e6f59b8cd696995782e0aa2d44edf120c8
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-10-29 21:47:25 +0800

    Todos state

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,18 @@ function App() {
   const [todo, setTodo] = useState<string>('');
   const [todos, setTodos] = useState<Todo []>([]);
 
+  const handleAdd = (e:React.FormEvent) =>{
+    e.preventDefault();
+
+    if(todo)
+    setTodos([...todos,{id: Date.now(),todo:todo, isDone:false}])
+    setTodo("")
+  }
+  
   return (
     <div className='App'>
       <span className='heading'>Todo List App</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
