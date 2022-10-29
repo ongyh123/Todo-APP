de6bea4c964d7dd3e2dcd470fab1ebacbfb3f12b
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-10-29 23:27:19 +0800

    reducer (to be completed)

--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,30 @@ export interface Todo {
     todo:string;
     isDone:boolean
 }
+
+// type Actions = 
+// |{type:'add', payload:string}
+// |{type:'remove', payload:number}
+// |{type:'done', payload:number}
+
+// const TodoReducer = (state:Todo[], action:Actions) => {
+//     switch (action.type) {
+//         case "add":
+//             return[...state,
+//             {id: Date.now(), todo: action.payload, isDone:false}];
+//         case "remove":
+//             return state.filter((todo)=> todo.id !==action.payload);
+//         case "done":
+//             return state.map((todo) =>
+//             todo.id !== action.payload? {...todo, isDone:!todo.isDone});
+//     }
+// }
+
+// import {useReducer} from 'react'
+
+// function ReducerExample = () => {
+
+//     const [state, dispatch] = useReducer(TodoReducer, [])
+
+//   return <div />;
+// }
